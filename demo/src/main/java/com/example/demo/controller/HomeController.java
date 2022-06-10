24c858a4143622f4027f470e3d2cdec83a0c6ada
옥승훈 <ock87@nate.com>
2022-06-10 15:23:48 +0900

    Spring boot 연습

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -19,6 +20,33 @@ public class HomeController {
 	public String helloMvc(@RequestParam("name") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello-templates";
+	}
+	
+	@GetMapping("hello-string")
+	// http에서 body부에 이 데이터를 직접 넣는다는 의미(ResponseBody)
+	@ResponseBody
+	public String helloString(@RequestParam("name") String name) {
+		return "hello" + name ;
+	}
+	
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Home helloApi(@RequestParam("name") String name) {
+		Home home = new Home();
+		home.setName(name);
+		return home;
+	}
+	
+	static class Home {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
 	}
 	
 	
